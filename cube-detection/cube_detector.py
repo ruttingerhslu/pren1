ad152b991f3fed4c8b75edd51a353fe4aa77ee70
Rafael Uttinger <rafael.uttinger@stud.hslu.ch>
2023-10-26 09:25:17 +0200

    feat: add cube detection draft

---
+++ cube-detection/cube_detector.py
@@ -0,0 +1,73 @@
+import cv2
+import numpy as np
+
+# blue
+lower_blue = np.array([90, 100, 100])
+upper_blue = np.array([135, 255, 255])
+
+# yellow
+lower_yellow = np.array([25, 100, 100])
+upper_yellow = np.array([35, 255, 255])
+
+# red1
+lower_red1 = np.array([0, 100, 100])
+upper_red1 = np.array([10, 255, 255])
+
+# red2
+lower_red2 = np.array([160, 100, 100])
+upper_red2 = np.array([180, 255, 255])
+
+def getFrame(file):
+    cap = cv2.VideoCapture(file)
+    cap.set(cv2.CAP_PROP_POS_FRAMES, 1000)
+    success, image = cap.read()
+    if success:
+        return image
+
+def filter_color(frame, lower_color, upper_color):
+    hsv = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
+    mask = cv2.inRange(hsv, lower_color, upper_color)
+    result = cv2.bitwise_and(frame, frame, mask=mask)
+    return result
+
+def edge_detection(frame):
+    gray = cv2.cvtColor(frame, cv2.COLOR_RGB2GRAY)
+    blurred = cv2.GaussianBlur(gray, (3, 3), 0)
+    canny = cv2.Canny(blurred, 20, 40)
+    kernel = np.ones((3,3), np.uint8)
+    dilated = cv2.dilate(canny, kernel, iterations=2)
+    return dilated
+
+def find_contours(image, edges):
+    contours, _ = cv2.findContours(edges, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+
+    # Loop over the contours to find cubes
+    for contour in contours:
+        epsilon = 0.02 * cv2.arcLength(contour, True)
+        approx = cv2.approxPolyDP(contour, epsilon, True)
+        # Check if the detected shape has 4 corners (a square or rectangle)
+        if len(approx) >= 4:
+            cv2.drawContours(image, [approx], -1, (0, 255, 0), 3)  # Draw the contour
+            if len(approx) > 11:
+                red1 = approx[5:]
+    return image
+
+def detect_color(frame, color):
+    match color:
+        case "blue":
+            color_mask = filter_color(frame, lower_blue, upper_blue)
+        case "yellow":
+            color_mask = filter_color(frame, lower_yellow, upper_yellow)
+        case "red":
+            color_mask = cv2.addWeighted(filter_color(frame, lower_red1, upper_red1), 1, filter_color(frame, lower_red2, upper_red2), 1, 0)
+    edges = edge_detection(color_mask)
+    contours = find_contours(frame, edges)
+
+    cv2.imwrite("edges_"+color+".jpg", contours)
+
+frame = getFrame('pren_cube_01.mp4')
+detect_color(frame, "blue")
+frame = getFrame('pren_cube_01.mp4')
+detect_color(frame, "yellow")
+frame = getFrame('pren_cube_01.mp4')
+detect_color(frame, "red")
---
+++ cube-detection/main.py
@@ -0,0 +1,50 @@
+import cv2
+import numpy as np
+
+def getFrame(file):
+    cap = cv2.VideoCapture(file)
+    cap.set(cv2.CAP_PROP_POS_FRAMES, 1000)
+    success, image = cap.read()
+    if success:
+        return image
+
+
+def edge_detection(frame):
+    # gray = cv2.cvtColor(frame, cv2.COLOR_RGB2GRAY)
+    blurred = cv2.GaussianBlur(frame, (3, 3), 0)
+    canny = cv2.Canny(blurred, 20, 40)
+    kernel = np.ones((3,3), np.uint8)
+    dilated = cv2.dilate(canny, kernel, iterations=2)
+    return dilated
+
+def find_contours(edges):
+    image = edges
+    contours, _ = cv2.findContours(edges, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+
+    # Loop over the contours to find cubes
+    for contour in contours:
+        epsilon = 0.04 * cv2.arcLength(contour, True)
+        approx = cv2.approxPolyDP(contour, epsilon, True)
+        print(approx)
+
+        # Check if the detected shape has 4 corners (a square or rectangle)
+        if len(approx) >= 4:
+            cv2.drawContours(image, [approx], -1, (0, 255, 0), 3)  # Draw the contour
+    return image
+
+
+frame = getFrame('pren_cube_01.mp4')
+gray1 = cv2.cvtColor(frame, cv2.COLOR_RGB2GRAY)
+gray2 = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+edge_detect1 = edge_detection(gray1)
+edge_detect2 = edge_detection(gray2)
+
+cv2.imwrite("img.jpg", frame)
+
+cv2.imwrite("edges1.jpg", edge_detect1)
+cv2.imwrite("edges2.jpg", edge_detect2)
+edge_detect = cv2.addWeighted(edge_detect1,1,edge_detect2,1,0)
+cv2.imwrite("edges.jpg", edge_detect)
+cv2.imwrite("contours.jpg", find_contours(edge_detect))
+
+cv2.imwrite("gray.jpg", cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY))
