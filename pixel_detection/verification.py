5d33d5727ba5203028e3eb237d8cc22837c872ee
denis.huelin <denis.huelin@stud.hslu.ch>
2024-03-14 11:06:14 +0100

    Add example RTSP Retrival script

--- pixel_detection/verification.py
+++ pixel_detection/verification.py
@@ -9,7 +9,21 @@ def send_cube_configuration_to_server(configuration):
     data = json.dumps(configuration)
     print(f"Requestbody sent to Server: {data}")
 
-    response = requests.post(url, headers=headers, data=data)
+    response = requests.post(url + "/config", headers=headers, data=data)
+    print(response)
+    print(f"Response Content: {response.content}")
+    return response
+
+
+def send_end_signal_to_server():
+    response = requests.post(url + "/end", headers=headers)
+    print(response)
+    print(f"Response Content: {response.content}")
+    return response
+
+
+def send_start_signal_to_server():
+    response = requests.post(url + "/start", headers=headers)
     print(response)
     print(f"Response Content: {response.content}")
     return response
