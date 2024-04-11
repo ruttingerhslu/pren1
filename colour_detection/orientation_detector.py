df8be12483083afb0c1553d901fc7bb9f0e2fa59
Rafael Uttinger <rafael.uttinger@stud.hslu.ch>
2024-04-11 16:47:41 +0200

    cube detection with opencv and stream as input

--- colour_detection/orientation_detector.py
+++ colour_detection/orientation_detector.py
@@ -1,6 +1,7 @@
 import cv2
 import numpy as np
 import math
+from matplotlib import pyplot as plt
 
 def getFrame(file, frame):
     cap = cv2.VideoCapture(file)
@@ -9,8 +10,64 @@ def getFrame(file, frame):
     if success:
         return image
 
+# blue
+lower_blue = np.array([90, 100, 100])
+upper_blue = np.array([135, 255, 255])
+
+# yellow
+lower_yellow = np.array([25, 100, 100])
+upper_yellow = np.array([35, 255, 255])
+
+# red1
+lower_red1 = np.array([0, 100, 100])
+upper_red1 = np.array([50, 255, 255])
+
+# red2
+lower_red2 = np.array([150, 100, 100])
+upper_red2 = np.array([180, 255, 255])
+
+def filter_color(frame, lower_color, upper_color):
+    hsv = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
+    mask = cv2.inRange(hsv, lower_color, upper_color)
+    result = cv2.bitwise_and(frame, frame, mask=mask)
+    return result
+
+def get_mask(frame, color):
+    match color:
+        case "blue":
+            return filter_color(frame, lower_blue, upper_blue)
+        case "yellow":
+            return filter_color(frame, lower_yellow, upper_yellow)
+        case "red":
+            return cv2.addWeighted(filter_color(frame, lower_red1, upper_red1), 1, filter_color(frame, lower_red2, upper_red2), 1, 0)
+
+def edge_detection(frame):
+    kernel = np.ones((12, 12), np.uint8)
+    eroded = cv2.erode(frame, kernel)
+    gray = cv2.cvtColor(eroded, cv2.COLOR_BGR2GRAY)
+    blurred = cv2.GaussianBlur(gray, (3, 3), 0)
+    canny = cv2.Canny(blurred, 0, 255)
+    kernel = np.ones((5,5), np.uint8)
+    dilated = cv2.dilate(canny, kernel, iterations=2)
+    return dilated
+
+def getCenterPoint(frame, color):
+    mask = get_mask(frame, color)
+    edges = edge_detection(mask)
+    # cv2.imshow('frame', edges)
+    contours, _ = cv2.findContours(edges, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+    for contour in contours:
+        # area = cv2.contourArea(contour)
+        # if area > 200:
+        M = cv2.moments(contour)
+        if M["m00"] != 0:
+            centroid_x = int(M["m10"] / M["m00"])
+            centroid_y = int(M["m01"] / M["m00"])
+            cv2.circle(frame, (centroid_x, centroid_y), 5, (0, 255, 0), -1)
+    cv2.imshow('frame', frame)
+
 def getMeanAngle(image):
-    lower_gray = np.array([150, 150, 150], dtype=np.uint8)
+    lower_gray = np.array([175, 175, 175], dtype=np.uint8)
     upper_gray = np.array([255, 255, 255], dtype=np.uint8)
 
     mask_gray = cv2.inRange(image, lower_gray, upper_gray)
@@ -39,7 +96,7 @@ def getMeanAngle(image):
 
                 angles.append(angle_deg)
                 # cv2.line(image, (centroid_x, centroid_y), (image_center_x, image_center_y), (255, 0, 0), 1)
-    # cv2.imwrite("output.png", image)
+    # cv2.imshow('frame', image)
     mean_angle = np.mean(angles)
     return mean_angle
 
@@ -68,7 +125,32 @@ def calculateCoordinates(angle, image):
     cv2.imwrite('img.png', image)
     return 0
 
-frame = 1000
-image = getFrame('../resources/pren_cube_01.mp4', frame)
-print("Mean angle: ", getMeanAngle(image))
-print(calculateCoordinates(getMeanAngle(image), image))
+def open_camera_profile(ip_address, username, password, profile):
+    # Open the camera
+    cap = cv2.VideoCapture('rtsp://' +
+                            username + ':' +
+                            password +
+                            '@' + ip_address +
+                            '/axis-media/media.amp' +
+                            '?streamprofile=' + profile)
+    if cap is None or not cap.isOpened():
+        print('Warning: unable to open video source: ', ip_address)
+        return None
+    while True:
+        ret, frame = cap.read()
+        if not ret:
+            print('Warning: unable to read next frame')
+            break
+        # getMeanAngle(frame)
+        # getCenterPoint(frame, "red")
+        
+        if cv2.waitKey(1) & 0xFF == ord('q'):
+            break
+
+open_camera_profile('147.88.48.131', 'pren', '463997',
+'pren_profile_med')
+
+# frame = 1000
+# image = getFrame('../resources/pren_cube_01.mp4', frame)
+# print("Mean angle: ", getMeanAngle(image))
+# print(calculateCoordinates(getMeanAngle(image), image))
--- cube-detection/cube_detector.py
+++ cube-detection/cube_detector.py
@@ -44,16 +44,16 @@ def edge_detection(frame):
     dilated = cv2.dilate(canny, kernel, iterations=2)
     return dilated
 
-def find_contours(image, edges):
+def find_contours(image, edges, sigma=0.02):
     contours, _ = cv2.findContours(edges, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
     # Loop over the contours to find cubes
     for contour in contours:
-        epsilon = 0.02 * cv2.arcLength(contour, True)
+        epsilon = sigma * cv2.arcLength(contour, True)
         approx = cv2.approxPolyDP(contour, epsilon, True)
         # Check if the detected shape has 4 corners (a square or rectangle)
         if len(approx) >= 4:
             cv2.drawContours(image, [approx], -1, (0, 255, 0), 3)  # Draw the contour
-            print(approx)
+            # print(approx)
     return image
 
 def detect_color(frame, color):
@@ -64,13 +64,78 @@ def detect_color(frame, color):
             color_mask = filter_color(frame, lower_yellow, upper_yellow)
         case "red":
             color_mask = cv2.addWeighted(filter_color(frame, lower_red1, upper_red1), 1, filter_color(frame, lower_red2, upper_red2), 1, 0)
-            cv2.imwrite('cube-detection/color_mask.jpg', color_mask)
+            # cv2.imwrite('cube-detection/color_mask.jpg', color_mask)
     edges = edge_detection(color_mask)
     contours = find_contours(frame, edges)
 
-    cv2.imwrite("cube-detection/edges_"+color+".jpg", contours)
+    return color_mask
+    # cv2.imwrite("cube-detection/edges_"+color+".jpg", contours)
 
-frame = getFrame('../resources/pren_cube_01.mp4')
-detect_color(frame.copy(), "blue")
-detect_color(frame.copy(), "yellow")
-detect_color(frame.copy(), "red")
+def getCenterPoint(image):
+    mask = 0
+    for color in ["blue", "yellow", "red"]:
+        mask += detect_color(image, color)
+    # edges = edge_detection(mask)
+    contours = find_contours(image, mask, 1)
+    cv2.imshow('contours', contours)
+
+def getMeanAngle(image):
+    lower_gray = np.array([0, 20, 200])
+    upper_gray = np.array([180, 50, 255])
+
+    hsv = cv2.cvtColor(image, cv2.COLOR_BGR2HSV)
+    mask_gray = cv2.inRange(hsv, lower_gray, upper_gray)
+
+    contours, _ = cv2.findContours(mask_gray, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+
+    angles = []
+
+    image_center_x = image.shape[1] // 2
+    image_center_y = image.shape[0] // 2
+
+    cv2.circle(image, (image_center_x, image_center_y), 5, (0, 255, 0), -1)
+
+    for contour in contours:
+        area = cv2.contourArea(contour)
+        if area > 1000:
+            M = cv2.moments(contour)
+            if M["m00"] != 0:
+                centroid_x = int(M["m10"] / M["m00"])
+                centroid_y = int(M["m01"] / M["m00"])
+
+                delta_x = centroid_x - image_center_x
+                delta_y =  image_center_y - centroid_y 
+                angle_rad = np.arctan2(delta_y, delta_x)
+                angle_deg = np.degrees(angle_rad)
+
+                angles.append(angle_deg)
+                cv2.line(image, (centroid_x, centroid_y), (image_center_x, image_center_y), (255, 0, 0), 1)
+    cv2.imshow('frame', image)
+    mean_angle = np.mean(angles)
+    return mean_angle
+
+def open_camera_profile(ip_address, username, password, profile):
+    # Open the camera
+    cap = cv2.VideoCapture('rtsp://' +
+                            username + ':' +
+                            password +
+                            '@' + ip_address +
+                            '/axis-media/media.amp' +
+                            '?streamprofile=' + profile)
+    if cap is None or not cap.isOpened():
+        print('Warning: unable to open video source: ', ip_address)
+        return None
+    while True:
+        ret, frame = cap.read()
+        if not ret:
+            print('Warning: unable to read next frame')
+            break
+        # getMeanAngle(frame)
+        # detect_color(frame, "blue")
+        getCenterPoint(frame)
+        
+        if cv2.waitKey(1) & 0xFF == ord('q'):
+            break
+
+open_camera_profile('147.88.48.131', 'pren', '463997',
+'pren_profile_med')
