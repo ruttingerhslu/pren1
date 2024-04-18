c50b2ee653a8b1825fc93f1f49edeb7b9b9407a8
Rafael Uttinger <rafael.uttinger@stud.hslu.ch>
2024-04-18 16:20:26 +0200

    working arrangement

---
+++ colour_detection/arrangement_ref.py
@@ -0,0 +1,43 @@
+# south
+{
+    (), # mid bottom
+    (), # right bottom
+    (), # UNKNOWN
+    (), # left bottom
+    (), # mid top (MIGHT BE WRONG)
+    (), # right top 
+    (), # top top
+    (), # left top
+}
+# west
+{
+    (), # left bottom #1
+    (), # mid bottom #2
+    (), # right bottom #3
+    (), # UNKNOWN #4
+    (), # left top #5
+    (), # mid top (MIGHT BE WRONG) #6
+    (), # right top #7
+    (), # top top #8
+}
+# north
+{
+    (), # UNKNOWN
+    (), # left bottom
+    (), # mid bottom
+    (), # right bottom
+    (), # top top
+    (), # left top 
+    (), # mid top (MIGHT BE WRONG)
+    (), # right top
+}
+# east
+{
+    (), # right bottom
+    (), # UNKNOWN
+    (), # left bottom
+    (), # mid bottom
+    (), # right top
+    (), # top top 
+    (), # left top
+    (), # mid top (MIGHT BE WRONG)
--- colour_detection/cube_calculator.py
+++ colour_detection/cube_calculator.py
@@ -2,10 +2,20 @@ import cv2
 import numpy as np
 import math
 
+color_ranges = {
+    "blue": (np.array([90, 100, 100]), np.array([135, 255, 255])),
+    "yellow": (np.array([25, 100, 100]), np.array([35, 255, 255])),
+    "red1": (np.array([0, 100, 100]), np.array([50, 255, 255])),
+    "red2": (np.array([150, 100, 100]), np.array([180, 255, 255]))
+}
+
+
 class CubeCalculator:
     def __init__(self) -> None:
         self._img = None
         self._gray = np.array([0, 0, 0])
+        self._center_x = 0
+        self._center_y = 0
         self.open_camera_profile('147.88.48.131', 'pren', '463997', 'pren_profile_med')
 
     def open_camera_profile(self, ip_address, username, password, profile):
@@ -26,8 +36,15 @@ class CubeCalculator:
             self._img = frame
             angle = self.getMeanAngle()
             if (math.isclose(abs(angle) % 90, 0, abs_tol = 1) or math.isclose(angle, 0, abs_tol = 1)):
-                cv2.imshow('good angle', frame)
-                print(self.getDirection(angle))
+                # cv2.imshow('good angle', frame)
+                # print(self.getDirection(angle))
+                direction = self.getDirection(angle)
+                points = self.getCubePoints()
+                arrangement = self.getArrangement(direction, points)
+                # print(arrangement)
+                # cv2.imwrite('arrangement.png', self._img)
+                config = self.getConfig(arrangement)
+                # print(config)
             if cv2.waitKey(1) & 0xFF == ord('q'):
                 break
 
@@ -42,9 +59,9 @@ class CubeCalculator:
 
         angles = []
 
-        image_center_x = image.shape[1] // 2
-        image_center_y = (image.shape[0] // 2) - 80
-        # cv2.circle(image, (image_center_x, image_center_y), 5, (0, 255, 0), -1)
+        self._center_x = image.shape[1] // 2
+        self._center_y = (image.shape[0] // 2) - 80
+        # cv2.circle(image, (self._center_x, self._center_y), 5, (0, 255, 0), -1)
 
         for contour in contours:
             area = cv2.contourArea(contour)
@@ -54,13 +71,13 @@ class CubeCalculator:
                     centroid_x = int(M["m10"] / M["m00"])
                     centroid_y = int(M["m01"] / M["m00"])
 
-                    delta_x = centroid_x - image_center_x
-                    delta_y =  image_center_y - centroid_y 
+                    delta_x = centroid_x - self._center_x
+                    delta_y =  self._center_y - centroid_y 
                     angle_rad = np.arctan2(delta_y, delta_x)
                     angle_deg = np.degrees(angle_rad)
 
                     angles.append(angle_deg)
-                    # cv2.line(image, (centroid_x, centroid_y), (image_center_x, image_center_y), (255, 0, 0), 1)
+                    # cv2.line(image, (centroid_x, centroid_y), (self._center_x, self._center_y), (255, 0, 0), 1)
         mean_angle = np.mean(angles)
         # cv2.putText(image, str(mean_angle), (20, 30), cv2.FONT_HERSHEY_SIMPLEX, 1, 2)
         return mean_angle
@@ -78,7 +95,73 @@ class CubeCalculator:
         elif angle >= 150 and angle <= 240:
             return 'west'
         else:
-            return 'unkown'
+            return 'unknown'
+
+    def getCubePoints(self):
+        image = self._img
+        a = 90
+
+        points = [[
+                (self._center_x - a, self._center_y), # left bottom
+                (self._center_x, int(self._center_y + a/2)), # mid bottom
+                (self._center_x + a, self._center_y), # right bottom
+                (), #UNKONWN (behind bottom)
+            ],
+            [
+                (self._center_x - a, self._center_y - a), # left top
+                (self._center_x, self._center_y - a), # mid top (NOT CERTAIN)
+                (self._center_x + a, self._center_y - a), # right top
+                (self._center_x, self._center_y - 2*a), # top top
+        ]]
+
+        # for layer in points:
+        #     for point in layer:
+        #         cv2.circle(image, point, 5, (0, 255, 0), -1)
+        return points
+        # cv2.imshow('test', image)
+
+    def getArrangement(self, orientation, points):
+        order = []
+        arrangement = []
+        match (orientation):
+            case 'west':
+                order = [0, 1, 2, 3]
+            case 'south':
+                order = [1, 2, 3, 0]
+            case 'east':
+                order = [2, 3, 0, 1]
+            case 'north':
+                order = [3, 0, 1, 2]
+        for i in range(0, 2):
+            for j in order:
+                arrangement.append(points[i][j])
+        return arrangement
+
+    def getConfig(self, arrangement):
+        image = self._img
+        config = {}
+        for index, point in enumerate(arrangement):
+            if point:
+                index += 1 # config starts at 1
+                config.update({ index : self.mapColor(point) })
+                cv2.circle(image, tuple(point), 5, (0, 255, 0), -1)
+                cv2.putText(image, str(index), tuple(point), cv2.FONT_HERSHEY_SIMPLEX, 1, 2)
+        cv2.imshow('img', image)
+        return config
+    
+    def mapColor(self, point):
+        image = self._img
+        hsv_img = cv2.cvtColor(image, cv2.COLOR_BGR2HSV)
+
+        hsv_point = hsv_img[tuple(reversed(point))]     
+
+        for color, (lower, upper) in color_ranges.items():
+            lower = np.array(lower)
+            upper = np.array(upper)
+            if np.all(hsv_point >= lower) and np.all(hsv_point <= upper):
+                return color
+        return "Unknown"
+
 
 if __name__ == '__main__':
     CubeCalculator()
